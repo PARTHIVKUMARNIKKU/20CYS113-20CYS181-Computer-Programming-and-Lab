c79b6dad3967a9f0c1eb526953a4688458e058db
PARTHIVKUMARNIKKU <116823475+PARTHIVKUMARNIKKU@users.noreply.github.com>
2023-04-10 09:52:21 +0530

    Add files via upload

---
+++ CB.EN.U4CYS22046/task_1_10_04_2023/sumofn.c
@@ -0,0 +1,11 @@
+/* This programme is to find the sum of n numbers  from 1 */
+#include <stdio.h>
+void main()
+{
+int n,sum;
+printf("enter the value of n for which you want the sum of n numbers \n ");
+scanf("%d", & n);
+sum=n*(n+1)/2;
+printf("the sum of the given n numbers is %d \n ",sum);
+}
+
