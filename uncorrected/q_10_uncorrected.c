82e93aa9f59a009873c305fa3daad8ab207d559a
parthiv kumar <116823475+PARTHIVKUMARNIKKU@users.noreply.github.com>
2023-06-22 15:22:00 +0530

    Add files via upload

---
+++ uncorrected/q_10_uncorrected.c
@@ -0,0 +1,20 @@
+struct complex_number{
+int var1,var2,var3;
+},s;
+void main()
+{
+int d,res,i_res;
+printf("enter the complex number");
+printf("a+ib");
+printf("enter a in the a+ib");
+scanf("%d",&var1);
+printf("enter b");
+scanf("%d",&s.var2);
+printf("enter c from c+id from the second number");
+scanf("%d"&s.var3);
+printf("enter the value d from the same above");
+scanf("%d",&d);
+res=((s.var1*s.var3)-(s.var2*d));
+i_res=(s.var2*s.var3);
+printf("the numeber is %d+i%d",res,i_res);
+}
